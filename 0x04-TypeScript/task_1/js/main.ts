cdc6107a1fca0b9f0d5cf4c29d5218ec197584a1
unknown <mightypz2@gmail.com>
2022-06-23 00:12:39 +0200

    Printing teachers

--- 0x04-TypeScript/task_1/js/main.ts
+++ 0x04-TypeScript/task_1/js/main.ts
@@ -11,3 +11,13 @@ interface Teacher {
 interface Directors extends Teacher {
     numberOfReports: number;
 }
+
+interface printTeacherFunction {
+    printTeacher(firstName: string, lastName: string): string;
+}
+
+function printTeacher(firstName: string, lastName: string) {
+    return (firstName[0] + ". " + lastName);
+}
+
+// console.log(printTeacher('John', "Doe"));
