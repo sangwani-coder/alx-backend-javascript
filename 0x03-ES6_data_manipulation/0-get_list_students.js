359a86833ab043210bec4d0937a1eb599b98d15c
zyambo <mightypz2@gmail.com>
2022-07-03 22:34:52 +0200

    fix typo

--- 0x03-ES6_data_manipulation/0-get_list_students.js
+++ 0x03-ES6_data_manipulation/0-get_list_students.js
@@ -1,9 +1,9 @@
 
 export default function getListStudents() {
 
-    const std1 = {id: 1, firstNmae: "Guillaume", location: "Francisco"};
-    const std2 = {id: 2, firstNmae: "James", location: "Columbia"};
-    const std3 = {id: 5, firstNmae: "Serena", location: "Francisco"};
+    const std1 = {id: 1, firstName: "Guillaume", location: "Francisco"};
+    const std2 = {id: 2, firstName: "James", location: "Columbia"};
+    const std3 = {id: 5, firstName: "Serena", location: "Francisco"};
 
     return new Array(std1, std2, std3);
 }
