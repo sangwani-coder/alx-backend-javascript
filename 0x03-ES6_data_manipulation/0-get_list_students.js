6daf6fad1e486fc24ecfb6715da5f90324858579
zyambo <mightypz2@gmail.com>
2022-07-03 23:28:29 +0200

    [Fix] Use array literal notation

--- 0x03-ES6_data_manipulation/0-get_list_students.js
+++ 0x03-ES6_data_manipulation/0-get_list_students.js
@@ -2,5 +2,5 @@ export default function getListStudents() {
   const std1 = { id: 1, firstName: 'Guillaume', location: 'San Francisco' };
   const std2 = { id: 2, firstName: 'James', location: 'Columbia' };
   const std3 = { id: 5, firstName: 'Serena', location: 'San Francisco' };
-  return new Array(std1, std2, std3);
+  return [std1, std2, std3];
 }
