aa210bdde4a272c3618243d164ab09f1c92a28f7
zyambo <mightypz2@gmail.com>
2022-07-03 22:20:39 +0200

    Fi

---
+++ 0x03-ES6_data_manipulation/0-get_list_students.js
@@ -0,0 +1,9 @@
+
+export default function getListStudents() {
+
+    const std1 = {id: 1, firstNmae: "Guillaume", location: "Francisco"};
+    const std2 = {id: 2, firstNmae: "James", location: "Columbia"};
+    const std3 = {id: 5, firstNmae: "Serena", location: "Francisco"};
+
+    return new Array(std1, std2, std3);
+}
