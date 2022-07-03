1be7035d2e6592b6788973557d4399cd49df52d8
zyambo <mightypz2@gmail.com>
2022-07-03 23:09:40 +0200

    [FIX] modify array constructor

--- 0x03-ES6_data_manipulation/0-get_list_students.js
+++ 0x03-ES6_data_manipulation/0-get_list_students.js
@@ -2,6 +2,6 @@ export default function getListStudents() {
     const std1 = { id: 1, firstName: "Guillaume", location: "San Francisco" };
     const std2 = { id: 2, firstName: "James", location: "Columbia" };
     const std3 = { id: 5, firstName: "Serena", location: "San Francisco" };
-
-    return new Array(std1, std2, std3);
+    const stds = [std1, std2, std3];
+    return stds
 }
