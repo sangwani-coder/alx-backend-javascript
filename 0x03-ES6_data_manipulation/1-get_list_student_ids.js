b67ccd73b7f3056a4f3a4f752016ed0a7b746cea
zyambo <mightypz2@gmail.com>
2022-07-04 00:05:51 +0200

    [fix] pass lint

--- 0x03-ES6_data_manipulation/1-get_list_student_ids.js
+++ 0x03-ES6_data_manipulation/1-get_list_student_ids.js
@@ -1,8 +1,9 @@
 /**
  * @param {string | any[]} students
  */
-export default function getListStudentIds(students){
-  if (Array.isArray(students))
-    return students.map((items) => items.id)
-  return []
+export default function getListStudentIds(students) {
+  if (Array.isArray(students)) {
+    return students.map((items) => items.id);
+}
+  return [];
 }
