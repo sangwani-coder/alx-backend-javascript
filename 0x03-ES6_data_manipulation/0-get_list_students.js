38583483b76cbcf7f3a170122be8985fd309b625
zyambo <mightypz2@gmail.com>
2022-07-03 22:42:27 +0200

    [FIX] Remove typo in location string

--- 0x03-ES6_data_manipulation/0-get_list_students.js
+++ 0x03-ES6_data_manipulation/0-get_list_students.js
@@ -1,9 +1,8 @@
-
 export default function getListStudents() {
 
-    const std1 = {id: 1, firstName: "Guillaume", location: "Francisco"};
+    const std1 = {id: 1, firstName: "Guillaume", location: "San Francisco"};
     const std2 = {id: 2, firstName: "James", location: "Columbia"};
-    const std3 = {id: 5, firstName: "Serena", location: "Francisco"};
+    const std3 = {id: 5, firstName: "Serena", location: "San Francisco"};
 
     return new Array(std1, std2, std3);
 }
