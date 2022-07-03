95e318d5dcea89f922213f907b1e8c98c25573c3
zyambo <mightypz2@gmail.com>
2022-07-04 00:01:15 +0200

    Add task 1

---
+++ 0x03-ES6_data_manipulation/1-get_list_student_ids.js
@@ -0,0 +1,8 @@
+/**
+ * @param {string | any[]} students
+ */
+export default function getListStudentIds(students){
+  if (Array.isArray(students))
+    return students.map((items) => items.id)
+  return []
+}
