c6e3e7b4cb8ca5b27a17e47b336ac2c09115b74d
zyambo <mightypz2@gmail.com>
2022-07-03 22:54:51 +0200

    revert to old code and and space before and after braces

--- 0x03-ES6_data_manipulation/0-get_list_students.js
+++ 0x03-ES6_data_manipulation/0-get_list_students.js
@@ -1,7 +1,7 @@
 export default function getListStudents() {
-    return [ 
-        {firstName: "Guillaume", id: 1, location: "San Francisco"},
-        {firstName: "James", id: 2, location: "Columbia"},
-        {firstName: "Serena", id: 5, location: "San Francisco"},
-    ];
+    const std1 = { id: 1, firstName: "Guillaume", location: "San Francisco" };
+    const std2 = { id: 2, firstName: "James", location: "Columbia" };
+    const std3 = { id: 5, firstName: "Serena", location: "San Francisco" };
+
+    return new Array(std1, std2, std3);
 }
