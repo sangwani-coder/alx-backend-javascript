9e121f16accbbb3a12d43cf3b4ea23fd7f797304
zyambo <mightypz2@gmail.com>
2022-07-04 00:42:43 +0200

    add task 3

---
+++ 0x03-ES6_data_manipulation/2-main.js
@@ -0,0 +1,6 @@
+import getStudentsByLocation from "./2-get_students_by_loc.js";
+import getListStudents from "./0-get_list_students.js";
+
+const students = getListStudents();
+
+console.log(getStudentsByLocation(students, 'San Francisco'));
