32ab931ad8fdc7a5357428e4748aef0dd7a2b3e6
zyambo <mightypz2@gmail.com>
2022-07-04 11:30:06 +0200

    [FIX] use lint spacing

--- 0x00-ES6_basic/0-constants.js
+++ 0x00-ES6_basic/0-constants.js
@@ -1,15 +1,14 @@
 export function taskFirst() {
-    var task = 'I prefer const when I can.';
-    return task;
-  }
-  
-  export function getLast() {
-    return ' is okay';
-  }
-  
-  export function taskNext() {
-    var combination = 'But sometimes let';
-    combination += getLast();
-  
-    return combination;
-  }
+  var task = 'I prefer const when I can.';
+  return task;
+}
+
+export function getLast() {
+  return ' is okay';
+}
+
+export function taskNext() {
+  var combination = 'But sometimes let';
+  combination += getLast();
+  return combination;
+}
