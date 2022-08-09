99f98f4daf234ad42de31646f4bd0fdbe030d32f
zyambo <mightypz2@gmail.com>
2022-08-10 00:50:24 +0200

    [FIX] use end to write data

--- 0x05-Node_JS_basic/4-http.js
+++ 0x05-Node_JS_basic/4-http.js
@@ -1,8 +1,8 @@
 const http = require('http');
 
 const app = http.createServer((req, res) => {
-    res.writeHead(200, {'Content-Type': 'text/html'});
-    res.write('Hello Holberton School!');
+    res.writeHead(200, {'Content-Type': 'text/plain'});
+    res.end('Hello Holberton School!');
 }).listen(1245);
 
 module.exports = app;
