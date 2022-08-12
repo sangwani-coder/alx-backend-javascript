591c0b1c7318b9abe2d31842d5da52dd9b3e0342
zyambo <mightypz2@gmail.com>
2022-08-12 21:13:07 +0200

    add task 6

--- 0x06-unittests_in_js/5-payment.js
+++ 0x06-unittests_in_js/5-payment.js
@@ -1,10 +1,4 @@
 
-// Create a new function named sendPaymentRequestToApi. The function takes two argument totalAmount,
-// and totalShipping
-// The function calls the Utils.calculateNumber function with type SUM
-//  totalAmount as a, totalShipping as b and display in the console 
-// the message The total is: <result of the sum></result>
-
 const Utils = require('./utils.js');
 
 module.exports = function sendPaymentRequestToApi(totalAmount, totalShipping) {
